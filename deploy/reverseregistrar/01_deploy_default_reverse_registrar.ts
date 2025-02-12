18692ab04bc18e378bf2cbea7dc21a8648ddcdec
tate <tate@ens.domains>
2025-02-12 18:42:02 +1100

    default rr deploy

--- deploy/reverseregistrar/01_deploy_default_reverse_registrar.ts
+++ deploy/reverseregistrar/01_deploy_default_reverse_registrar.ts
@@ -22,8 +22,12 @@ const func: DeployFunction = async function (hre) {
   const registry = await viem.getContract('ENSRegistry')
   const root = await viem.getContract('Root')
 
+  const currentRootOwner = await root.read.owner()
   const currentReverseOwner = await registry.read.owner([namehash('reverse')])
-  if (currentReverseOwner !== owner.address) {
+  if (
+    currentRootOwner === owner.address &&
+    currentReverseOwner !== owner.address
+  ) {
     const setReverseOwnerHash = await root.write.setSubnodeOwner(
       [labelhash('reverse'), owner.address],
       { account: owner.account },
@@ -32,6 +36,11 @@ const func: DeployFunction = async function (hre) {
       `Setting owner of .reverse to owner on root (tx: ${setReverseOwnerHash})...`,
     )
     await viem.waitForTransactionSuccess(setReverseOwnerHash)
+  } else if (currentRootOwner !== owner.address) {
+    console.warn(
+      'Root owner account not available, skipping default.reverse setup on registry',
+    )
+    return
   }
 
   const setResolverHash = await registry.write.setSubnodeRecord(
@@ -52,8 +61,8 @@ const func: DeployFunction = async function (hre) {
   await viem.waitForTransactionSuccess(setResolverHash)
 }
 
-func.id = 'reverse-registrar'
-func.tags = ['ReverseRegistrar']
+func.id = 'default-reverse-registrar'
+func.tags = ['DefaultReverseRegistrar']
 func.dependencies = ['root']
 
 export default func
