b04545c287ff51a7f40a3cdb39f28617a36cb804
tate <tate@ens.domains>
2025-05-26 10:32:11 +1000

    formatting + moved duration check to makeCommitment

--- contracts/ethregistrar/ETHRegistrarController.sol
+++ contracts/ethregistrar/ETHRegistrarController.sol
@@ -1,6 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ~0.8.17;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
 import {BaseRegistrarImplementation} from "./BaseRegistrarImplementation.sol";
 import {StringUtils} from "../utils/StringUtils.sol";
 import {Resolver} from "../resolvers/Resolver.sol";
@@ -9,10 +13,6 @@ import {IReverseRegistrar} from "../reverseRegistrar/IReverseRegistrar.sol";
 import {IDefaultReverseRegistrar} from "../reverseRegistrar/IDefaultReverseRegistrar.sol";
 import {ReverseClaimer} from "../reverseRegistrar/ReverseClaimer.sol";
 import {IETHRegistrarController, IPriceOracle} from "./IETHRegistrarController.sol";
-
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {INameWrapper} from "../wrapper/INameWrapper.sol";
 import {ERC20Recoverable} from "../utils/ERC20Recoverable.sol";
 
@@ -27,48 +27,87 @@ contract ETHRegistrarController is
     using StringUtils for *;
     using Address for address;
 
+    /// @notice The minimum duration for a registration.
     uint256 public constant MIN_REGISTRATION_DURATION = 28 days;
 
+    // @notice The node (i.e. namehash) for the eth TLD.
     bytes32 private constant ETH_NODE =
         0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae;
 
+    /// @notice The maximum expiry time for a registration.
     uint64 private constant MAX_EXPIRY = type(uint64).max;
 
+    /// @notice The ENS registry.
     ENS public immutable ens;
 
+    // @notice The base registrar implementation for the eth TLD.
     BaseRegistrarImplementation immutable base;
 
+    /// @notice The price oracle for the eth TLD.
     IPriceOracle public immutable prices;
 
+    /// @notice The minimum time a commitment must exist to be valid.
     uint256 public immutable minCommitmentAge;
 
+    /// @notice The maximum time a commitment can exist to be valid.
     uint256 public immutable maxCommitmentAge;
 
+    /// @notice The registrar for addr.reverse. (i.e. reverse for coinType 60)
     IReverseRegistrar public immutable reverseRegistrar;
 
+    /// @notice The registrar for default.reverse. (i.e. fallback reverse for all EVM chains)
     IDefaultReverseRegistrar public immutable defaultReverseRegistrar;
 
+    /// @notice A mapping of commitments to their timestamp.
     mapping(bytes32 => uint256) public commitments;
 
+    /// @notice Thrown when a commitment is not found.
     error CommitmentNotFound(bytes32 commitment);
+
+    /// @notice Thrown when a commitment is too new.
     error CommitmentTooNew(
         bytes32 commitment,
         uint256 minimumCommitmentTimestamp,
         uint256 currentTimestamp
     );
+
+    /// @notice Thrown when a commitment is too old.
     error CommitmentTooOld(
         bytes32 commitment,
         uint256 maximumCommitmentTimestamp,
         uint256 currentTimestamp
     );
+
+    /// @notice Thrown when a name is not available to register.
     error NameNotAvailable(string name);
+
+    /// @notice Thrown when the duration supplied for a registration is too short.
     error DurationTooShort(uint256 duration);
+
+    /// @notice Thrown when data is supplied for a registration without a resolver.
     error ResolverRequiredWhenDataSupplied();
+
+    /// @notice Thrown when a matching unexpired commitment exists.
     error UnexpiredCommitmentExists(bytes32 commitment);
+
+    /// @notice Thrown when the value sent for a registration is insufficient.
     error InsufficientValue();
+
+    /// @notice Thrown when the maximum commitment age is too low.
     error MaxCommitmentAgeTooLow();
+
+    /// @notice Thrown when the maximum commitment age is too high.
     error MaxCommitmentAgeTooHigh();
 
+    /// @notice Emitted when a name is registered.
+    ///
+    /// @param label The label of the name.
+    /// @param labelhash The keccak256 hash of the label.
+    /// @param owner The owner of the name.
+    /// @param baseCost The base cost of the name.
+    /// @param premium The premium cost of the name.
+    /// @param expires The expiry time of the name.
+    /// @param referrer The referrer of the registration.
     event NameRegistered(
         string label,
         bytes32 indexed labelhash,
@@ -78,6 +117,14 @@ contract ETHRegistrarController is
         uint256 expires,
         bytes32 referrer
     );
+
+    /// @notice Emitted when a name is renewed.
+    ///
+    /// @param label The label of the name.
+    /// @param labelhash The keccak256 hash of the label.
+    /// @param cost The cost of the name.
+    /// @param expires The expiry time of the name.
+    /// @param referrer The referrer of the registration.
     event NameRenewed(
         string label,
         bytes32 indexed labelhash,
@@ -86,6 +133,15 @@ contract ETHRegistrarController is
         bytes32 referrer
     );
 
+    /// @notice Constructor for the ETHRegistrarController.
+    ///
+    /// @param _base The base registrar implementation for the eth TLD.
+    /// @param _prices The price oracle for the eth TLD.
+    /// @param _minCommitmentAge The minimum time a commitment must exist to be valid.
+    /// @param _maxCommitmentAge The maximum time a commitment can exist to be valid.
+    /// @param _reverseRegistrar The registrar for addr.reverse.
+    /// @param _defaultReverseRegistrar The registrar for default.reverse.
+    /// @param _ens The ENS registry.
     constructor(
         BaseRegistrarImplementation _base,
         IPriceOracle _prices,
@@ -110,6 +166,11 @@ contract ETHRegistrarController is
         defaultReverseRegistrar = _defaultReverseRegistrar;
     }
 
+    /// @notice Returns the price of a registration for the given label and duration.
+    ///
+    /// @param label The label of the name.
+    /// @param duration The duration of the registration.
+    /// @return price The price of the registration.
     function rentPrice(
         string calldata label,
         uint256 duration
@@ -118,10 +179,18 @@ contract ETHRegistrarController is
         price = _rentPrice(label, labelhash, duration);
     }
 
+    /// @notice Returns true if the label is valid for registration.
+    ///
+    /// @param label The label to check.
+    /// @return True if the label is valid, false otherwise.
     function valid(string calldata label) public pure returns (bool) {
         return label.strlen() >= 3;
     }
 
+    /// @notice Returns true if the label is valid and available for registration.
+    ///
+    /// @param label The label to check.
+    /// @return True if the label is valid and available, false otherwise.
     function available(
         string calldata label
     ) public view override returns (bool) {
@@ -129,14 +198,25 @@ contract ETHRegistrarController is
         return _available(label, labelhash);
     }
 
+    /// @notice Returns the commitment for a registration.
+    ///
+    /// @param registration The registration to make a commitment for.
+    /// @return commitment The commitment for the registration.
     function makeCommitment(
         Registration calldata registration
     ) public pure override returns (bytes32 commitment) {
         if (registration.data.length > 0 && registration.resolver == address(0))
             revert ResolverRequiredWhenDataSupplied();
+
+        if (registration.duration < MIN_REGISTRATION_DURATION)
+            revert DurationTooShort(registration.duration);
+
         return keccak256(abi.encode(registration));
     }
 
+    /// @notice Commits a registration.
+    ///
+    /// @param commitment The commitment to commit.
     function commit(bytes32 commitment) public override {
         if (commitments[commitment] + maxCommitmentAge >= block.timestamp) {
             revert UnexpiredCommitmentExists(commitment);
@@ -144,6 +224,16 @@ contract ETHRegistrarController is
         commitments[commitment] = block.timestamp;
     }
 
+    /// @notice Registers a name.
+    ///
+    /// @param registration The registration to register.
+    /// @param registration.label The label of the name.
+    /// @param registration.owner The owner of the name.
+    /// @param registration.duration The duration of the registration.
+    /// @param registration.resolver The resolver for the name.
+    /// @param registration.data The data for the name.
+    /// @param registration.reverseRecord The reverse record for the name.
+    /// @param registration.referrer The referrer of the registration.
     function register(
         Registration calldata registration
     ) public payable override {
@@ -181,9 +271,6 @@ contract ETHRegistrarController is
             revert NameNotAvailable(registration.label);
 
         delete (commitments[commitment]);
-
-        if (registration.duration < MIN_REGISTRATION_DURATION)
-            revert DurationTooShort(registration.duration);
 
         bytes32 namehash = keccak256(abi.encodePacked(ETH_NODE, labelhash));
         uint256 expires;
@@ -247,6 +334,11 @@ contract ETHRegistrarController is
             payable(msg.sender).transfer(msg.value - totalPrice);
     }
 
+    /// @notice Renews a name.
+    ///
+    /// @param label The label of the name.
+    /// @param duration The duration of the registration.
+    /// @param referrer The referrer of the registration.
     function renew(
         string calldata label,
         uint256 duration,
@@ -269,10 +361,12 @@ contract ETHRegistrarController is
             payable(msg.sender).transfer(msg.value - price.base);
     }
 
+    /// @notice Withdraws the balance of the contract to the owner.
     function withdraw() public {
         payable(owner()).transfer(address(this).balance);
     }
 
+    /// @inheritdoc IERC165
     function supportsInterface(
         bytes4 interfaceID
     ) external pure returns (bool) {
